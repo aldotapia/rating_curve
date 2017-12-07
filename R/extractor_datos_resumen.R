c563ac5b354359896e2b1d4a60f435f9cc5ab196
aldotapia <aldo.tapia.a@gmail.com>
2017-12-07 15:39:57 -0300

    Ficha de terreno v0.91, iterador en .R, extractor datos relevantes

--- R/extractor_datos_resumen.R
+++ R/extractor_datos_resumen.R
@@ -19,11 +19,31 @@ nombres <- gsub(pattern = '.xlsx',replacement = '',x = nombres)
 # Establecer directorio de trabajo
 setwd(Rnw_path)
 
+Table_list <- list()
+
 # Bucle para exportar archivos
 for (l in 1:length(archivos)) { # uso de l para iterar... Rnw usa i y j como iterados y cambia variables globales
   archivo <- archivos[l] # Selección de archivo
-  Sweave('Ficha_reporte.Rnw') # Creación de archivo LaTeX
-  system(paste0('pdflatex ',Rnw_path,'Ficha_reporte.tex')) # Compilación en .pdf
-  file.rename('Ficha_reporte.pdf',paste0('Nro_', l,'_alt_',gsub('.','_',alturas[l],fixed = T), '.pdf')) # Cambio de nombre según uno personalizado
-  print(paste0(nombres[l],' listo!')) # Imprimir mensaje de que está listo (aunque entre tanto mensaje se confunde)
+  
+  wb <- loadWorkbook(archivo)
+  
+  hoja <- readWorksheet(wb,sheet = 'Cálculo Aforo',colTypes = XLC$DATA_TYPE.NUMERIC,forceConversion = T)
+  hoja_h <- readWorksheet(wb,sheet = 'Hidráulica',colTypes = XLC$DATA_TYPE.STRING,forceConversion = T)
+  
+  # Extraer datos
+  Caudal <- hoja[21,13]
+  Area <- hoja[61,6]
+  Velocidad <- hoja[63,6]
+  Prof_h <- as.numeric(hoja_h[13,9])
+  Froude <- Velocidad/sqrt(9.81*Prof_h) # Cálculo del número de Froude (ya que la planilla sólo informa a 1 decimal)
+  
+  # crear data.frame por planilla
+  Table_list[[l]] <- data.frame(ID = l, Altura = alturas[l], Caudal = Caudal,
+                               Area = Area, Velocidad = Velocidad, Froude = Froude)
 }
+
+# Juntar los resultados de todas las planillas
+Tabla_resumen <- do.call(rbind.data.frame,Table_list)
+
+# Exportar tabla
+write.csv(Tabla_resumen, '/path/to/rating_curve/Data/Results/Tabla_resumen.csv')
