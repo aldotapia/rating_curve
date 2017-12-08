ff22d762fe6656a441cb0e4c438e15f2528ad494
aldotapia <aldo.tapia.a@gmail.com>
2017-12-08 00:06:11 -0300

    Avance extractor

--- R/extractor_datos_resumen.R
+++ R/extractor_datos_resumen.R
@@ -1,8 +1,8 @@
 # Indicar ruta doonde se encuentren las planillas (Puede ser raíz, el proceso es recursivo)
-files_path <- '/path/to/rating_curve/Data/Sheets'
+files_path <- '~/Desktop/Proyectos/rating_curve/Data/Sheets'
 
 # Indicar rutra donde se encuentra el archivo Ficha_reporte.Rnw
-Rnw_path <- '/path/to/rating_curve/Sweave_terreno/'
+Rnw_path <- '~/Desktop/Proyectos/rating_curve/Sweave_terreno/'
 
 # Cargar ruta de archivos
 archivos <- list.files(files_path,pattern = '.xlsx$',recursive = T,full.names = T)
@@ -29,21 +29,26 @@ for (l in 1:length(archivos)) { # uso de l para iterar... Rnw usa i y j como ite
   
   hoja <- readWorksheet(wb,sheet = 'Cálculo Aforo',colTypes = XLC$DATA_TYPE.NUMERIC,forceConversion = T)
   hoja_h <- readWorksheet(wb,sheet = 'Hidráulica',colTypes = XLC$DATA_TYPE.STRING,forceConversion = T)
+  hoja_c <- readWorksheet(wb,sheet = 'Cálculo Aforo',colTypes = XLC$DATA_TYPE.STRING,forceConversion = T)
   
   # Extraer datos
+  Fecha <- ifelse(is.na(hoja_c[7,3]),'nd',substr(hoja_c[7,3],1,10))
   Caudal <- hoja[21,13]
   Area <- hoja[61,6]
   Velocidad <- hoja[63,6]
   Prof_h <- as.numeric(hoja_h[13,9])
   Froude <- Velocidad/sqrt(9.81*Prof_h) # Cálculo del número de Froude (ya que la planilla sólo informa a 1 decimal)
+  Este <-  hoja[10,3]
+  Norte <-  hoja[11,3]
   
   # crear data.frame por planilla
-  Table_list[[l]] <- data.frame(ID = l, Altura = alturas[l], Caudal = Caudal,
-                               Area = Area, Velocidad = Velocidad, Froude = Froude)
+  Table_list[[l]] <- data.frame(ID = l, Fecha = Fecha, Altura = alturas[l], Caudal = Caudal,
+                               Area = Area, Velocidad = Velocidad, Froude = Froude,
+                               Este = Este, Norte = Norte)
 }
 
 # Juntar los resultados de todas las planillas
 Tabla_resumen <- do.call(rbind.data.frame,Table_list)
 
 # Exportar tabla
-write.csv(Tabla_resumen, '/path/to/rating_curve/Data/Results/Tabla_resumen.csv')
+write.csv(Tabla_resumen, '~/Desktop/Proyectos/rating_curve/Data/Results/Tabla_resumen.csv')
